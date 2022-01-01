46bbe8ce485b112029cde1f92385780990e4211f
Kenneth Adjei <[kmadjei25@gmail.com]>
2022-01-01 16:22:24 -0500

    storing data in memory and making request argument variable required

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {"ken": {"age": 19, "gender": "male"},
-        "bill": {"age":70, "gender": "male"}}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", 
+    required=True)
+video_put_args.add_argument("name", type=int, help="Views of the video", 
+    required=True)
+video_put_args.add_argument("name", type=int, help="Likes of the video", 
+    required=True)
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
+videos = {}
 
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[]
+
+api.add_resource(video, "/video/<int: video_id>")
 
 if __name__ == '__main__':
     app.run(debug=True)
