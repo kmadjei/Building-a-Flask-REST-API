36c0412b86504564d2d067940289b5775cf5a799
Kenneth Adjei <[kmadjei25@gmail.com]>
2022-01-05 07:24:46 -0500

    completed tutorial by Tec with time https://youtu.be/GMppyAPbLYk?list=PLIjfLg4gyecLdZK_gKz7SFYWUN7GbKsuZ

--- test.py
+++ test.py
@@ -2,18 +2,14 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [
-    {"likes": 78, "name": "Joe", "views": 100000},
-    {"likes": 100000, "name": "How to Make REST API", "views": 80000},
-    {"likes": 780, "name": "Ken greatness", "views": 2000}
-]
-
-for i in range (len(data)):
-    response = requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
+# data = [
+#     {"likes": 78, "name": "Joe", "views": 100000},
+#     {"likes": 100000, "name": "How to Make REST API", "views": 80000},
+#     {"likes": 780, "name": "Ken greatness", "views": 2000}
+# ]
 
 
-input()
-response = requests.get(BASE + "video/6")
+response = requests.patch(BASE + "video/2", {"view": 99})
 # prints serializable JSON fromatted string
 print(response.json())
+
