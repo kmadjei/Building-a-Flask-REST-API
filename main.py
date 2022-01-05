1c606a64319e304ceaf00df74b06e8b8b6991574
Kenneth Adjei <[kmadjei25@gmail.com]>
2022-01-05 06:18:53 -0500

    hadel delete, creating a model, and querying a database

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name = {name}, views = {views}, likes = {likes})"
+
+#db.create_all()  # run once in order to create db model
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", 
@@ -12,15 +26,7 @@ video_put_args.add_argument("views", type=int, help="Views of the video",
 video_put_args.add_argument("likes", type=int, help="Likes of the video", 
     required=True)
 
-videos = {}
 
-def abort_if_video_id_doesnt_exist(video_id):
-    if video_id not in videos:
-        abort(404, message="could not find video...")
-
-def abort_if_video_exists(video_id):
-    if video_id in videos:
-        abort(409, message="Video already exists with that ID...")
 
 class Video(Resource):
     def get(self, video_id):
